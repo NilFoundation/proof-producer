4320572cd24b1d85bbbdd99e818c8b1c5589dd89
nkaskov <mail.nikita.kaskov@gmail.com>
2023-12-29 22:12:42 +0000

    CPack added for single-threaded proof-generator.

---
+++ cmake/create-symlinks.cmake
@@ -0,0 +1,12 @@
+message("Creating symlinks")
+
+list(APPEND BINARIES proof-generator)
+
+file(MAKE_DIRECTORY $ENV{DESTDIR}/usr/bin)
+
+foreach(BINARY IN LISTS BINARIES)
+    set(LINK_NAME $ENV{DESTDIR}/usr/bin/${BINARY})
+    set(LINK_TARGET "../lib/zkllvm/bin/${BINARY}")
+    message("${LINK_NAME} -> ${LINK_TARGET}")
+    file(CREATE_LINK ${LINK_TARGET} ${LINK_NAME} SYMBOLIC)
+endforeach()
