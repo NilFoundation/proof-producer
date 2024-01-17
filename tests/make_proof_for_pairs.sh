7efaf16df1c51889741b27c88ca534fd76e30b49
x-mass <36629999+x-mass@users.noreply.github.com>
2024-01-17 14:53:04 +0000

    Fix GH Actions build

--- tests/make_proof_for_pairs.sh
+++ tests/make_proof_for_pairs.sh
@@ -10,18 +10,17 @@ get_script_dir() {
 make_proof_for_pair() {
     local tbl_file=$1
     local crct_file="${tbl_file%assignment.tbl}circuit.crct"
-    local relative_path="$(realpath --relative-to="$base_dir" "$tbl_file")"
-    # Updated to use the specified output directory
-    local proof_dir="${output_dir}/$(dirname ${relative_path})"
+    local relative_tbl_path="$(realpath --relative-to="$base_dir" "$tbl_file")"
+    local proof_dir="${output_dir}/$(dirname ${relative_tbl_path})"
 
-    local proof_generator_binary="${script_dir}/../build/bin/proof-generator/proof-generator"
+    local proof_generator_binary="${script_dir}/../build/bin/proof-generator-single-threaded/proof-generator-single-threaded"
     if [ "$use_multithreaded" = true ]; then
         proof_generator_binary="${script_dir}/../build/bin/proof-generator-multi-threaded/proof-generator-multi-threaded"
     fi
 
     if [ -f "$crct_file" ]; then
         mkdir -p "$proof_dir"  # Ensure the output directory exists
-        echo -n "Processing $tbl_file and $crct_file (proof will be at $relative_path): "
+        echo -n "Processing $tbl_file and $crct_file (proof will be at $proof_dir): "
         if $proof_generator_binary -t "$tbl_file" --circuit "$crct_file" --proof "$proof_dir/proof.bin"; then
             color_green "success"
         else
@@ -72,13 +71,11 @@ script_dir=$(get_script_dir)
 clean=false
 parse_args "$@"
 
-# Validate output directory
 if [ -z "$output_dir" ]; then
     echo "Output directory not specified. Using default."
     output_dir=$base_dir
 fi
 
-# Function to process files in a directory
 process_directory() {
     local dir=$1
     while read tbl_file; do
@@ -94,7 +91,6 @@ if [ ${#targets[@]} -gt 0 ]; then
         process_directory "$base_dir/$target"
     done
 else
-    # Process the base directory
     process_directory "$base_dir"
 fi
 
