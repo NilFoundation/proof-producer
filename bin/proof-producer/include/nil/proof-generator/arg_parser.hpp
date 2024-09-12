debacb142b40a464271c34ebbe0d0d06e6cc09a1
Iluvmagick <d.tabalin@nil.foundation>
2024-09-12 16:30:56 +0400

    Added challenge aggregation stage.

--- bin/proof-producer/include/nil/proof-generator/arg_parser.hpp
+++ bin/proof-producer/include/nil/proof-generator/arg_parser.hpp
@@ -43,6 +43,8 @@ namespace nil {
             boost::filesystem::path circuit_file_path;
             boost::filesystem::path assignment_table_file_path;
             boost::filesystem::path assignment_description_file_path;
+            std::vector<boost::filesystem::path> input_challenge_files;
+            boost::filesystem::path aggregated_challenge_file = "aggregated_challenge.dat";
             boost::log::trivial::severity_level log_level = boost::log::trivial::severity_level::info;
             CurvesVariant elliptic_curve_type = type_identity<nil::crypto3::algebra::curves::pallas>{};
             HashesVariant hash_type = type_identity<nil::crypto3::hashes::keccak_1600<256>>{};
